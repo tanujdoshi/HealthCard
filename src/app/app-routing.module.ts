140ca0867802756679bc4ebda516b34af442003d
Bhavik Mojidra <mbhavik273@gmail.com>
2020-01-19 13:00:10 +0530

    Router Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 ``;
 
-const routes: Routes = [{ path: "/Login", component: LoginComponent }];
+const routes: Routes = [{ path: "Login", component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
