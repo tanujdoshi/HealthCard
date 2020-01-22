97d8e7c6f90ac82adb2dd0b93fcb490ea93825b2
tanujdoshi <50872198+tanujdoshi@users.noreply.github.com>
2020-01-22 23:44:17 +0530

    after patient

---
+++ server/schemas/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+var userschemas = new mongoose.Schema({
+   name:String,
+    password:String,
+    address:String,
+    contact:String,
+    dob:Date,
+    blood:String,
+    email:String
+   
+});
+var user = mongoose.model("user", userschemas);
+
+module.exports = user;
